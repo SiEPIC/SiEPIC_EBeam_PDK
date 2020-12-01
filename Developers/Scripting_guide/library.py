6e25751f44e8256ae95db7118dc1eedfeecbe0d7
Mustafa <mustafa@ece.ubc.ca>
2020-12-01 10:02:45 -0800

    Passives 2020 PCell tutorial

---
+++ Developers/Scripting_guide/library.py
@@ -0,0 +1,6 @@
+import math
+from SiEPIC.utils import get_technology, get_technology_by_name
+
+# Import KLayout Python API methods:
+# Box, Point, Polygon, Text, Trans, LayerInfo, etc
+from pya import *
