4679ef46f3df9127695e874821702554a0c70344
jasminabrar <jasminabrarr@gmail.com>
2023-08-29 10:37:22 -0700

    Cleaning up the script
    Former-commit-id: f585b3aee44c574e6b22cad12a1985d78361e0f1

--- klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
+++ klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
@@ -2,7 +2,6 @@ import pya
 from pya import *
 import os
 import pathlib
-#import sys
 
 #import pcells_EBeam ### folder name ###
 import importlib
@@ -21,15 +20,14 @@ ebeam_layout = pya.Library().library_by_id(ebeam_id).layout()
 error_occured = False
 
 for f in files:
-        mm = f.replace('.py','')# Example: mm = Waveguide
+        mm = f.replace('.py','')
         
         # check pcell has been registered in layout
         if mm not in ebeam_layout.pcell_names():
-             #pya.MessageBox.critical("PCell Registration Error", "PCell {} could not be registered".format(mm), pya.MessageBox.Ok)
              print("PCell Registration Error. PCell {} could not be registered".format(mm))
              error_occured = True
               
-        # instantiate pCell in new layer and check that it contains polygons
+        # instantiate pcell in new layer and check that it contains polygons
         new_layout = pya.Layout()
         pcell_decl = ebeam_layout.pcell_declaration(mm)
         new_layout.register_pcell(mm, pcell_decl)
@@ -43,7 +41,6 @@ for f in files:
         pcell = new_layout.create_cell(mm, all_params)
             
         if pcell.is_empty():
-             #pya.MessageBox.critical("PCell Insantiation Error", "PCell {} is empty when instantiated in a new layout".format(mm), pya.MessageBox.Ok)
              print("PCell Insantiation Error. PCell {} is empty when instantiated in a new layout".format(mm))
              error_occured = True
 
@@ -52,9 +49,6 @@ completion_messages = ["All pcells from pcells_EBeam folder were successfully re
                        "Some pcells from pcells_EBeam folder were unsuccessfully registered in EBeam library."]
 
 print("Complete. " + completion_messages[error_occured])
-
-#with open(os.path.join(os.path.dirname(__file__), 'hello_world.txt'), 'w') as f:
-    #f.write('hello world')
 
         
 
