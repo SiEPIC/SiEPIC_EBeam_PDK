e13f2dfd95e181d114530bf7fbd6e95d7f3345a8
Fariha Shaikh <fariha@voltsafe.com>
2023-11-14 10:54:25 -0800

    Fixed missing library name ebeam dream

--- klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
+++ klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
@@ -6,7 +6,7 @@ import pathlib
 #import pcells_EBeam ### folder name ###
 import importlib
 
-# Equivalent to from custom_exceptions_path import *
+# Equivalent to "from custom_exceptions_path import *"
 current_directory = os.path.dirname(os.path.abspath(__file__))
 custom_exceptions_path = os.path.join(current_directory, 'custom_exceptions.py')
 exec(open(custom_exceptions_path).read())
@@ -28,55 +28,57 @@ by Jasmina Brar
     - tests successful pcell registration in all libraries 
 """
 
-library_folders = ["pcells_EBeam", "pcells_EBeam_Beta", "pcells_SiN"]
-library_names = ["EBeam", "EBeam_Beta", "EBeam-SiN"]
-tech_names = ["EBeam", "EBeam", "EBeam"]
+library_folders = ["pcells_EBeam", "pcells_EBeam_Beta", "pcells_SiN", "pcells_EBeam_Dream"]
+library_names = ["EBeam", "EBeam_Beta", "EBeam-SiN", "EBeam-Dream"]
+tech_names = ["EBeam", "EBeam", "EBeam", "EBeam"]
 
 for i in range(len(library_folders)): 
 
-    # get all .py files in library folder
+    # Get all .py files in library folder
     files = [f for f in os.listdir(os.path.join(os.path.dirname(
         os.path.realpath(__file__)),library_folders[i])) if '.py' in pathlib.Path(f).suffixes  and '__init__' not in f]
     
     importlib.invalidate_caches()
-    
-    # get library layout object 
+
+    # Get library layout object 
     tech_name = tech_names[i]
     library_name = library_names[i]
     library = pya.Library().library_by_name(library_name,tech_name)
-    
+
+    print('*** Testing library: %s' % library_name)
+
     # With self hosted runner, EBeam library is not being initialized, skip over it
     if library == None:
         break;
         
     layout = library.layout()
-    
-    # check that the library is registered in klayout
+
+    # Check that the library is registered in klayout
     try:
         if layout == None:
             raise LibraryNotRegistered(library_name)
-        
+
     except LibraryNotRegistered as e:
         print("Caught {}: {}".format(type(e).__name__, str(e)))
         pya.Application.instance().exit(1)
-    
-    # loop through all pcells from this library's folder
+
+    # Loop through all pcells from this library's folder
     for f in files:
-    
+        
         try: 
             mm = f.replace('.py','')
-            
-            # check that the pcell has been registered in the library's layout
+
+            # Check that the pcell has been registered in the library's layout
             if mm not in layout.pcell_names():
                 raise PCellRegistrationError(mm, library_name) 
-                
-            # instantiate pcell in a new layer and check that it contains polygons
+
+            # Instantiate pcell in a new layer and check that it contains polygons
             new_layout = pya.Layout()
             pcell_decl = layout.pcell_declaration(mm)
             new_layout.register_pcell(mm, pcell_decl)
-                
+
             parameter_decl = pcell_decl.get_parameters()
-            
+
             all_params = {}
             for p in parameter_decl:
                 all_params[p.name] = p.default
@@ -90,7 +92,7 @@ for i in range(len(library_folders)):
         except (PCellRegistrationError, PCellInstantiationError) as e:
             print("Caught {}: {}".format(type(e).__name__, str(e)))
             pya.Application.instance().exit(1)
-    
-    print("Complete. All pcells from {} folder were successfully registered in {} library".format(library_folders[i], library_names[i]))
 
+    print("Complete. All pcells from {} folder were successfully registered in {} library".format(library_folders[i], library_names[i]))
+    
 print("Complete. All pcells were succcessfully registered in all libraries.")
