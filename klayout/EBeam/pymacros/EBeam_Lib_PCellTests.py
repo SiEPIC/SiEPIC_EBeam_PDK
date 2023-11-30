57c87524da8aa484746828c3d9c2d1e8a478456a
jasminabrar <jasminabrarr@gmail.com>
2023-11-30 12:12:39 -0800

    custom_exceptions.py
    script will now catch errors in pcell script

--- klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
+++ klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
@@ -89,6 +89,13 @@ for i in range(len(library_folders)):
                 all_params[p.name] = p.default
             
             pcell = new_layout.create_cell(mm, all_params)
+
+            # check that there were no errors generated from the pcell
+            
+            error_shapes = pcell.shapes(new_layout.error_layer())
+
+            for error in error_shapes.each():
+                raise PCellImplementationError(mm, library_name, error.text)
                 
             if pcell.is_empty() or pcell.bbox().area() == 0:
                 raise PCellInstantiationError(mm, library_name)
@@ -96,7 +103,6 @@ for i in range(len(library_folders)):
             #topcell = new_layout.create_cell("top")
             #t = Trans(Trans.R0, 0,0)
             #inst = topcell.insert(CellInstArray(pcell.cell_index(), t))
-            
         
         except (PCellRegistrationError, PCellInstantiationError, Exception) as e:
             print("Caught {}: {}".format(type(e).__name__, str(e)))
