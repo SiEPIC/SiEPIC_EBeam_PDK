30b3e30f814a021f295b60e6306e65a187faa78a
Mustafa Hammood <mustafa@siepic.com>
2024-10-04 20:34:58 -0700

    make reference waveguide optional toggle

--- klayout/EBeam/pymacros/pcells_EBeam_Dream/ebeam_dream_microlens_edge_couplers_BB.py
+++ klayout/EBeam/pymacros/pcells_EBeam_Dream/ebeam_dream_microlens_edge_couplers_BB.py
@@ -1,7 +1,9 @@
 import pya
 import math
-from pya import *
-
+from SiEPIC._globals import PIN_LENGTH as pin_length
+from SiEPIC.extend import to_itype
+from SiEPIC.scripts import path_to_waveguide, connect_pins_with_waveguide, connect_cell
+from SiEPIC.utils import get_technology_by_name, load_Waveguides_by_Tech, get_layout_variables
 
 class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
     """
@@ -14,7 +16,7 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
 
         # Important: initialize the super class
         super(ebeam_dream_microlens_edge_couplers_BB, self).__init__()
-        from SiEPIC.utils import get_technology_by_name, load_Waveguides_by_Tech, get_layout_variables
+
         self.technology_name = 'EBeam'
         TECHNOLOGY = get_technology_by_name(self.technology_name)
 
@@ -24,9 +26,10 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
         p.add_choice('1550 nm', '1550')
         p.add_choice('1310 nm', '1310')
 
+        self.param("ref_wg", self.TypeBoolean, "Include reference waveguide", default=True)
+
         #declare the layers
-        self.param("silayer", self.TypeLayer, "Si Layer", default = TECHNOLOGY['Si'])
-        self.param("waveguidelayer",self.TypeLayer, "Waveguide Layer", default = TECHNOLOGY['Waveguide'])
+        self.param("silayer", self.TypeLayer, "Si Layer", default = TECHNOLOGY['Si'], hidden=True)
         self.param("pinrec", self.TypeLayer, "PinRec Layer", default = TECHNOLOGY['PinRec'])
         self.param("devrec", self.TypeLayer, "DevRec Layer", default = TECHNOLOGY['DevRec'])
         self.param("fibertarget", self.TypeLayer, "Fiber Target Layer", default=TECHNOLOGY['FbrTgt'])
@@ -39,18 +42,9 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
 
     def produce(self, layout, layers, parameters, cell):
         # This is the main part of the implementation: create the layout
-
         self.cell = cell
         self._param_values = parameters
         self.layout = layout
-
-        # import neccessary libraries
-        from math import pi, sin, cos, tan
-        from SiEPIC._globals import PIN_LENGTH as pin_length
-        from SiEPIC.extend import to_itype
-        from SiEPIC.scripts import path_to_waveguide, connect_pins_with_waveguide, connect_cell
-
-        import ctypes
 
         #fetch the parameters
         dbu = self.layout.dbu
@@ -61,7 +55,6 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
         LayerPinRecN = ly.layer(self.pinrec)
         LayerDevRecN = ly.layer(self.devrec)
         LayerFbrTgtN = ly.layer(self.fibertarget)
-        LayerWN = ly.layer(self.waveguidelayer)
         LayerTEXTN = ly.layer(self.textl)
         LayerBBN = ly.layer(self.bb)
 
@@ -94,30 +87,29 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
             theta = 2*math.pi/npts
             pts = []
             for i in range(0,npts):
-              pts.append(Point.from_dpoint(pya.DPoint((x+r*math.cos(i*theta))/1,(y+r*math.sin(i*theta))/1)))
+              pts.append(pya.Point.from_dpoint(pya.DPoint((x+r*math.cos(i*theta))/1,(y+r*math.sin(i*theta))/1)))
             return pts
 
         #draw one loopback device
-        for ref_loop in range(2):
+        if self.ref_wg:
+            for ref_loop in range(2):
+                #draw fibre target circle
+                align_circle = circle(offset,-pitch*(ref_loop+1),2/dbu)
+                #place fibre target circle
+                shapes(LayerFbrTgtN).insert(pya.Polygon(align_circle))
 
-            #draw fibre target circle
-            align_circle = circle(offset,-pitch*(ref_loop+1),2/dbu)
-
-            #place fibre target circle
-            shapes(LayerFbrTgtN).insert(Polygon(align_circle))
-
-        #create waveguide to for loopback
-        loopback_path = DPath([DPoint(0,-127),DPoint((offset + l_taper + Lw2)*dbu+15,-127),DPoint((offset + l_taper + Lw2)*dbu+15,-254),DPoint(0,-254)],0.5)
-        self.layout.technology_name = self.technology_name #required otherwise "create_cell" doesn't load
-        pcell = self.layout.create_cell("Waveguide",self.technology_name,{"path": loopback_path, "waveguide_type": waveguide_type})
-        t = Trans(Trans.R0,0,0)
-        self.cell.copy(pcell,LayerSiN,LayerBBN)
-        wg = self.cell.insert(CellInstArray(pcell.cell_index(),t))
-        wg.flatten()
-        self.cell.clear(LayerDevRecN)
-        self.cell.clear(LayerPinRecN)
-        self.cell.clear(LayerSiN)
-        self.cell.clear(LayerWN)
+        if self.ref_wg:
+            #create waveguide to for loopback
+            loopback_path = pya.DPath([pya.DPoint(0,-127),pya.DPoint((offset + l_taper + Lw2)*dbu+15,-127),pya.DPoint((offset + l_taper + Lw2)*dbu+15,-254),pya.DPoint(0,-254)],0.5)
+            self.layout.technology_name = self.technology_name #required otherwise "create_cell" doesn't load
+            pcell = self.layout.create_cell("Waveguide",self.technology_name,{"path": loopback_path, "waveguide_type": waveguide_type})
+            t = pya.Trans(pya.Trans.R0,0,0)
+            self.cell.copy(pcell,LayerSiN,LayerBBN)
+            wg = self.cell.insert(pya.CellInstArray(pcell.cell_index(),t))
+            wg.flatten()
+            self.cell.clear(LayerDevRecN)
+            self.cell.clear(LayerPinRecN)
+            self.cell.clear(LayerSiN)
 
 
         ##########################################################################################################################################################################
@@ -125,18 +117,18 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
         for n_ch in range(int(num_channels)):
 
             #draw the taper
-            taper_pts = [Point(0,-w_waveguide/2+pitch*n_ch),Point(0,w_waveguide/2+pitch*n_ch),Point(offset + l_taper + Lw3,w_waveguide/2+pitch*n_ch),Point(offset + l_taper + Lw3,-w_waveguide/2+pitch*n_ch)]
+            taper_pts = [pya.Point(0,-w_waveguide/2+pitch*n_ch),pya.Point(0,w_waveguide/2+pitch*n_ch),pya.Point(offset + l_taper + Lw3,w_waveguide/2+pitch*n_ch),pya.Point(offset + l_taper + Lw3,-w_waveguide/2+pitch*n_ch)]
 
             #place the taper
-            shapes(LayerBBN).insert(Polygon(taper_pts))
+            shapes(LayerBBN).insert(pya.Polygon(taper_pts))
 
             #draw and place pin on the waveguide:
             x = offset + l_taper + Lw3
-            t = Trans(Trans.R0, x, pitch*n_ch)
-            pin = Path([Point(-pin_length/2,0),Point(pin_length/2,0)], w_waveguide)
+            t = pya.Trans(pya.Trans.R0, x, pitch*n_ch)
+            pin = pya.Path([pya.Point(-pin_length/2,0),pya.Point(pin_length/2,0)], w_waveguide)
             pin_t = pin.transformed(t)
             shapes(LayerPinRecN).insert(pin_t)
-            text = Text(f"opt{n_ch+1}",t)
+            text = pya.Text(f"opt{n_ch+1}",t)
             shape = shapes(LayerPinRecN).insert(text)
             shape.text_size = 3/dbu
    
@@ -144,61 +136,62 @@ class ebeam_dream_microlens_edge_couplers_BB(pya.PCellDeclarationHelper):
             align_circle = circle(offset,pitch*n_ch,2/dbu)
 
             #place fibre target circle
-            shapes(LayerFbrTgtN).insert(Polygon(align_circle))
+            shapes(LayerFbrTgtN).insert(pya.Polygon(align_circle))
 
         #draw devrec box
-        devrec_pts = [Point(0,pitch*n_ch+30/dbu),Point(x,pitch*n_ch+30/dbu),Point(x,-pitch*2-30/dbu),Point(0,-pitch*2-30/dbu)]
+        if self.ref_wg:
+            devrec_pts = [pya.Point(0,pitch*n_ch+30/dbu),pya.Point(x,pitch*n_ch+30/dbu),pya.Point(x,-pitch*2-30/dbu),pya.Point(0,-pitch*2-30/dbu)]
+        else:
+            devrec_pts = [pya.Point(0,pitch*n_ch+30/dbu),pya.Point(x,pitch*n_ch+30/dbu),pya.Point(x,-30/dbu),pya.Point(0,-30/dbu)]
         
         #place devrec box
-        shapes(LayerDevRecN).insert(Polygon(devrec_pts))
+        shapes(LayerDevRecN).insert(pya.Polygon(devrec_pts))
 
         #edge of chip text
-        t = Trans(Trans.R0,0,-110/dbu)
-        text = Text("<- Edge of chip",t)
+        t = pya.Trans(pya.Trans.R0,0,1/dbu)
+        text = pya.Text("<- Edge of chip",t)
         shape = shapes(LayerTEXTN).insert(text)
         shape.text_size = 3/dbu
 
         #BB description
-        t = Trans(Trans.R0,0,-95/dbu)
-        text = Text("  Number of Channel(s): " + str(num_channels) + "\n  Center Wavelength: " + str(wavelength) + " nm",t)
+        t = pya.Trans(pya.Trans.R0,0,-15/dbu)
+        text = pya.Text("  Number of Channel(s): " + str(num_channels) + "\n  Center Wavelength: " + str(wavelength) + " nm",t)
         shape = shapes(LayerTEXTN).insert(text)
-        shape.text_size = 4/dbu
+        shape.text_size = 3/dbu
 
         #BB description
-        t = Trans(Trans.R0,5/dbu,-240/dbu)
-        text = Text("     Reference Waveguide -->\n\n\n\n\n\n<-- 25 MFD lens",t)
+        t = pya.Trans(pya.Trans.R0, 0,-25/dbu)
+        text = pya.Text("<- 25 MFD lens",t)
         shape = shapes(LayerTEXTN).insert(text)
         shape.text_size = 3/dbu
 
         #draw DP BB logo
         import os
         dir_path = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../gds/EBeam_Dream/"))
-        filename = os.path.join(dir_path, 'DP_Edge_coupler_for_PWB_BB_logo.gds')
-        tech_name = 'EBeam'
+        filename = os.path.join(dir_path, 'DP_lens_BB_logo.gds')
+
         ly2=pya.Layout()
         ly2.read(filename)
         top_cell = ly2.top_cell()
         top_cell.flatten(True)
-        #top_cell.Trans(Trans.R0,0,-192)
-        #self.cell.copy_shapes(top_cell)
         self.cell.copy_shapes(top_cell)
 
         #draw lenses
         width_lens = to_itype(50, dbu)
         length_lens = to_itype(50, dbu)
-        
-        for n_ch in range(int(num_channels+2)):
-            #draw the taper
-            taper_pts = [Point(0,-width_lens/2+pitch*n_ch-2*pitch), Point(0,width_lens/2+pitch*n_ch-2*pitch), Point(-length_lens,width_lens/2+pitch*n_ch-2*pitch),Point(-length_lens,-width_lens/2+pitch*n_ch-2*pitch)]
 
-            #place the taper
-            shapes(LayerBBN).insert(Polygon(taper_pts))
-   
-            #draw fibre target circle
-            align_circle = circle(-length_lens,pitch*n_ch-2*pitch,25/dbu)
-
-            #place fibre target circle
-            shapes(LayerBBN).insert(Polygon(align_circle))
+        if self.ref_wg:
+            for n_ch in range(int(num_channels+2)):
+                lens_pts = [pya.Point(0,-width_lens/2+pitch*n_ch-2*pitch), pya.Point(0,width_lens/2+pitch*n_ch-2*pitch), pya.Point(-length_lens,width_lens/2+pitch*n_ch-2*pitch),pya.Point(-length_lens,-width_lens/2+pitch*n_ch-2*pitch)]
+                shapes(LayerBBN).insert(pya.Polygon(lens_pts))
+                lens = circle(-length_lens,pitch*n_ch-2*pitch,25/dbu)
+                shapes(LayerBBN).insert(pya.Polygon(lens))
+        else:
+            for n_ch in range(int(num_channels)):
+                lens_pts = [pya.Point(0,-width_lens/2+pitch*n_ch), pya.Point(0,width_lens/2+pitch*n_ch), pya.Point(-length_lens,width_lens/2+pitch*n_ch),pya.Point(-length_lens,-width_lens/2+pitch*n_ch)]
+                shapes(LayerBBN).insert(pya.Polygon(lens_pts))
+                lens = circle(-length_lens,pitch*n_ch,25/dbu)
+                shapes(LayerBBN).insert(pya.Polygon(lens))
 
     def display_text_impl(self):
         # Provide a descriptive text for the cell
